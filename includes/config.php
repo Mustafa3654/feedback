9b6cf8ad027b4a8510c1fa793eec276857a96640
kayed-b <22320012@students.liu.edu.lb>
2026-05-17 22:35:29 +0300

    main

--- includes/config.php
+++ includes/config.php
@@ -7,7 +7,7 @@
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'feedback_portal');
 define('DB_USER', 'root');
-define('DB_PASS', 'Mostafa2004'); // Default XAMPP has no password
+define('DB_PASS', ''); // Default XAMPP has no password
 
 // Error reporting (set to 0 in production)
 error_reporting(E_ALL);
